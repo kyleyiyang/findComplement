a39f1a4afa26ae2fd23b6c739f9f8692c2a01dc4
kyleyiyang <kyleyiyang@gmail.com>
2020-10-17 15:36:46 -0400

    Update findComplement.java

--- findComplement.java
+++ findComplement.java
@@ -15,6 +15,23 @@ public class Main
 		System.out.println(result);
 	}
 }
+// no improvement
+int count=0;
+		int result=0;
+        if ((Math.log(num) / Math.log(2))==(int)(Math.log(num) / Math.log(2))) {
+            return (num-1);
+        } else {
+		//while (num/2>0) {
+            while (num/2>0) {
+            if (num%2==0) {
+                result+=(int)Math.pow(2,count);
+            }
+            //num>>1;
+            num=(int) Math.floor((double)(num/2));
+		    count++;
+		}
+        }
+		 return result;
 // this is slower
 class Solution {
     public int findComplement(int num) {
